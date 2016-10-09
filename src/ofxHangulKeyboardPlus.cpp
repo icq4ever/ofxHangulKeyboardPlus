07de628207151ba2879b32ddf4c455500098e5da
Yi donghoon <icq4ever@gmail.com>
2016-10-10 06:59:42 +0900

    키보드매니저 추가

--- src/ofxHangulKeyboardPlus.cpp
+++ src/ofxHangulKeyboardPlus.cpp
@@ -1,6 +1,6 @@
 //
 //  ofxHangulKeyboardPlus.cpp
-//  ofxHangulKeyboardPlusExample
+//  ofxHangulKeyboardPlus
 //
 //  Created by icq4ever on 10/10/2016.
 //
@@ -9,5 +9,56 @@
 #include "ofxHangulKeyboardPlus.h"
 
 ofxHangulKeyboardPlus::ofxHangulKeyboardPlus(){
+	hangulKeyboard = new softHangulKeyboard();
+	engKeyboard = new softHangulKeyboard();
+	
+	hangulKeyboard->activate();
+	engKeyboard->deActivate();
 
+}
+
+ofxHangulKeyboardPlus::~ofxHangulKeyboardPlus(){
+	delete hangulKeyboard;
+	delete engKeyboard;
+
+}
+
+void ofxHangulKeyboardPlus::setup(ofBaseApp *_ofApp, bool hangulMode){
+	ofApp = _ofApp;
+	hangulKeyboard->setup(ofApp, OFXSK_LAYOUT_KEYBOARD_FULL_KR);
+	engKeyboard->setup(ofApp, OFXSK_LAYOUT_KEYBOARD_FULL_EN);
+	
+	if(hangulMode)	isHangulMode = true;
+	else			isHangulMode = false;
+}
+
+void ofxHangulKeyboardPlus::sendKey(int _key){
+	hangulKeyboard->keyInput(_key);
+	engKeyboard->keyInput(_key);
+}
+
+void ofxHangulKeyboardPlus::update(){
+	tmpBuffer_kr = hangulKeyboard->getBuffer();
+	tmpBuffer_en = engKeyboard->getBuffer();
+}
+
+void ofxHangulKeyboardPlus::draw(){
+	if(hangulKeyboard->isGetActivated())	hangulKeyboard->draw(100, 350);
+	if(engKeyboard->isGetActivated())		engKeyboard->draw(100, 350);
+}
+
+void ofxHangulKeyboardPlus::toggleKeyboard(){
+	hangulKeyboard->toggleInputLanguage();
+	engKeyboard->toggleInputLanguage();
+	
+	// 키보드가 비 활성화 됐을때 fullBuffer에 string을 가져오고, 키보드의 버퍼를 삭제한다.
+}
+
+string ofxHangulKeyboardPlus::getBuffer(){
+	return mergedBuffer;
+}
+
+void ofxHangulKeyboardPlus::clearBuffer(){
+	// 버퍼 삭제
+	mergedBuffer.clear();
 }
--- src/ofxHangulKeyboardPlus.h
+++ src/ofxHangulKeyboardPlus.h
@@ -1,29 +1,41 @@
 //
 //  ofxHangulKeyboardPlus.h
-//  ofxHangulKeyboardPlusExample
+//  ofxHangulKeyboardPlus
 //
 //  Created by icq4ever on 10/10/2016.
 //
 //
 
-#ifndef __ofxHangulKeyboardPlusExample__ofxHangulKeyboardPlus__
-#define __ofxHangulKeyboardPlusExample__ofxHangulKeyboardPlus__
-
 #include "ofMain.h"
+#include "softHangulKeyboard.h"
 
 class ofxHangulKeyboardPlus{
 
 public:
 	
-	static ofxHangulKeyboardPlus& one(){
-		static ofxHangulKeyboardPlus instance; // Instantiated on first use.
-		return instance;
-	}
-
 	ofxHangulKeyboardPlus();
+	~ofxHangulKeyboardPlus();
+	
+	void setup(ofBaseApp *_ofApp, bool hangulMode);
+	void update();
+	void draw();
+	void sendKey(int key);
+	void toggleKeyboard();
+	string getBuffer();
+	void clearBuffer();
 	
 protected:
-
+	ofBaseApp	*ofApp;
+	bool isHangulMode;
+	
+	string tmpBuffer_kr;
+	string tmpBuffer_en;
+	string mergedBuffer;
+	
+	softHangulKeyboard *hangulKeyboard;
+	softHangulKeyboard *engKeyboard;
+	
+	ofVec2f positiion;
+	float lastKeyComboCheckTimer;
 };
 
-#endif /* defined(__ofxHangulKeyboardPlusExample__ofxHangulKeyboardPlus__) */
